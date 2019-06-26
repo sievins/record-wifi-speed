e852610496fbdc48dcf54665a9b27d76a7121300
Samuel Ievins <sievins8012@gmail.com>
2019-06-26 10:37:30 +0100

    Only record speed if connected to specified network

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
 const path = require('path')
 const shell = require('shelljs')
 const moment = require('moment')
+const wifiName = require('wifi-name')
+require('dotenv').config()
 
 const { exec, ShellString } = shell
 
-// pkg knows to include speed-test as an asset by parsing path.join
-const speedTestPath = path.join(__dirname, 'node_modules/speed-test/cli.js')
-const speedTestCommand = `node ${speedTestPath} -j`
-
-exec(speedTestCommand, (code, stdout) => {
-  const result = JSON.parse(stdout)
-
-  const now = moment()
-  const day = now.format('DD/MM/YYYY')
-  const time = now.format('HH:mm')
-
-  const record = {
-    ...result,
-    day,
-    time,
+wifiName().then(name => {
+  if (name === process.env.WIFI_NAME) {
+    speedTest()
   }
-
-  writeRecord(record)
 })
+
+const speedTest = () => {
+  // pkg knows to include speed-test as an asset by parsing path.join
+  const speedTestPath = path.join(__dirname, 'node_modules/speed-test/cli.js')
+  const speedTestCommand = `node ${speedTestPath} -j`
+
+  exec(speedTestCommand, (code, stdout) => {
+    const result = JSON.parse(stdout)
+
+    const now = moment()
+    const day = now.format('DD/MM/YYYY')
+    const time = now.format('HH:mm')
+
+    const record = {
+      ...result,
+      day,
+      time,
+    }
+
+    writeRecord(record)
+  })
+}
 
 const writeRecord = (record) => {
   const output = `${JSON.stringify(record)}\n`
-  const location = 'C:\\Users\\Sam\\Projects\\internet-speed\\results\\record.txt'
-
-  ShellString(output).toEnd(location)
+  ShellString(output).toEnd(process.env.RECORD_LOCATION)
 }
