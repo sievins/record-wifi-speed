66ed632b7cb6c95e0a90d1f42da1b5d6ac7a7dda
Samuel Ievins <sievins8012@gmail.com>
2019-07-17 15:30:10 +0100

    Add title to generated charts

--- src/charts/histogram/generate-charts.js
+++ src/charts/histogram/generate-charts.js
@@ -8,8 +8,19 @@ module.exports = ({ resultsDirectory }) => {
   const chartsDirectory = resolve(resultsDirectory, './charts')
   mkdirSync(chartsDirectory, { recursive: true })
 
+  const container = (title) => `
+    <div id="container">
+      <h2>${title}</h2>
+      <div id="chart"></div>
+    </div>
+  `
   const options = { width: 960, height: 600 }
-  output(resolve(chartsDirectory, './download'), d3nBar({ data: download }), options)
-  output(resolve(chartsDirectory, './upload'), d3nBar({ data: upload }), options)
-  output(resolve(chartsDirectory, './ping'), d3nBar({ data: ping }), options)
+
+  const downloadBarChart = d3nBar({ data: download, container: container('Download speed') })
+  const uploadBarChart = d3nBar({ data: upload, container: container('Upload speed') })
+  const pingBarChart = d3nBar({ data: ping, container: container('Ping speed') })
+
+  output(resolve(chartsDirectory, './download'), downloadBarChart, options)
+  output(resolve(chartsDirectory, './upload'), uploadBarChart, options)
+  output(resolve(chartsDirectory, './ping'), pingBarChart, options)
 }
